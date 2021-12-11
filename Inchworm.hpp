e2a7834f6182e0eb3e2a2eb62421795239d0ff47
Eunyoung Choi <70963337+eunyooung@users.noreply.github.com>
2021-12-11 23:34:52 +1030

    Inchworm Solution

---
+++ Inchworm.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+class Inchworm
+{
+	public:
+	int lunchtime(int branch, int rest, int leaf) {
+		int count = 0;
+		for(int i = 0; i <= branch; i += rest){
+			if( i % leaf == 0 ) count++;
+		}
+		return count;
+	}
+};
